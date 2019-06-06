707800c5d8c40c7b663a4d9d203c168a8222b071
vixo341 <malpa341@gmail.com>
2019-06-06 15:27:39 +0200

    Project Setup

---
+++ FacebookApplication/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FacebookApplication.Startup))]
+
+namespace FacebookApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
